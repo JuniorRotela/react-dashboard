fc9e51d53c86188222b5c8e64c1f79a633a6f6e4
Junior Dario Rotela Fretes <juniorrotela@MacBook-Pro-de-Junior.local>
2024-02-27 15:35:45 -0300

    ok

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,5 +1,21 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material"; 
+import {Routes, Route} from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+// import Invoices from "./scenes/invoices";
+// import Team from "./scenes/team";
+// import Contacts from "./scenes/contacts";
+// import Bar from "./scenes/bar";
+// import Form from "./scenes/form";
+// import Line from "./scenes/line";
+// import Pie from "./scenes/pie";
+// import Faq from "./scenes/faq";
+// import Geography from "./scenes/geography";
+// import Calendar from "./scenes/calendar";
+
+
 
 function App() {
   const [theme, ColorMode] = useMode();
@@ -9,8 +25,23 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline/>
       <div className="app">
-        <main className="content"></main>
-        
+        <Sidebar/>
+        <main className="content">
+          <Topbar/>
+          <Routes>
+            <Route path="/" element={<Dashboard/>} />
+            {/* <Route path="/team" element={<Team/>} /> */}
+            {/* <Route path="/invoices" element={<Invoices/>} /> */}
+            {/* <Route path="/contacts" element={<Contacts/>} /> */}
+            {/* <Route path="/bar" element={<Bar/>} /> */}
+            {/* <Route path="/form" element={<Form/>} /> */}
+            {/* <Route path="/line" element={<Line/>} /> */}
+            {/* <Route path="/pie" element={<Pie/>} /> */}
+            {/* <Route path="/faq" element={<Faq/>} /> */}
+            {/* <Route path="/geography" element={<Geography/>} /> */}
+            {/* <Route path="/calendar" element={<Calendar/>} /> */}
+          </Routes>
+        </main>
         </div>;
       </ThemeProvider>
     </ColorModeContext.Provider>

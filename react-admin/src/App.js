9d24e2f669bf53a69b5a8ea12b248a7978dc0275
Junior Dario Rotela Fretes <juniorrotela@MacBook-Pro-de-Junior.local>
2024-03-09 11:29:18 -0300

    ok

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -8,12 +8,12 @@ import Invoices from "./scenes/invoices";
  import Team from "./scenes/team";
  import Contacts from "./scenes/contacts";
  import Form from "./scenes/form";
-// import Bar from "./scenes/bar";
-// import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
-// import Faq from "./scenes/faq";
-// import Geography from "./scenes/geography";
-// import Calendar from "./scenes/calendar";
+ import Bar from "./scenes/bar";
+ import Line from "./scenes/line";
+import Pie from "./scenes/pie";
+ import Faq from "./scenes/faq";
+ import Geography from "./scenes/geography";
+import Calendar from "./scenes/calendar";
 
 
 
@@ -33,13 +33,13 @@ function App() {
             <Route path="/team" element={<Team/>} /> 
             <Route path="/invoices" element={<Invoices/>} /> 
             <Route path="/contacts" element={<Contacts/>} /> 
-            {/* <Route path="/bar" element={<Bar/>} /> */}
+             <Route path="/bar" element={<Bar/>} /> 
             <Route path="/form" element={<Form/>} />
-            {/* <Route path="/line" element={<Line/>} /> */}
-            {/* <Route path="/pie" element={<Pie/>} /> */}
-            {/* <Route path="/faq" element={<Faq/>} /> */}
-            {/* <Route path="/geography" element={<Geography/>} /> */}
-            {/* <Route path="/calendar" element={<Calendar/>} /> */}
+            <Route path="/line" element={<Line/>} /> 
+             <Route path="/pie" element={<Pie/>} />
+           <Route path="/faq" element={<Faq/>} /> 
+             <Route path="/geography" element={<Geography/>} /> 
+            <Route path="/calendar" element={<Calendar/>} /> 
           </Routes>
         </main>
         </div>;
